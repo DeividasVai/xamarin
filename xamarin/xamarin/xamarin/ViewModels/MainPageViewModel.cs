d3855ed9850de169cafc77287561d4e30926923c
Deividas Vaičiūnas <deva@columbusglobal.com>
2019-05-17 16:31:30 +0300

    Initial working copy of a xamarin cross-platform mvvm project

---
+++ xamarin/xamarin/xamarin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace xamarin.ViewModels
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel()
+        {
+            Title = "Laba diena";
+        }
+    }
+}
